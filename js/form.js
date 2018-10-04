22172c8fbc0b1256b261d6d506948bd93b3ee8df
alinessilva <alinedoze@gmail.com>
2018-10-04 20:10:25 -0300

    Refatoração - Add função que constroi as tags td e tr do paciente

--- js/form.js
+++ js/form.js
@@ -6,28 +6,9 @@ var form = document.getElementById("add-form");
 
 // Extrai info do paciente do form
 var patient = getPatientForm(form);
-console.log(patient);
 
 // Cria a td e tr do paciente
-var patientTr = document.createElement("tr");
-
-var nameTd = document.createElement("td");
-var weightTd = document.createElement("td");
-var heightTd = document.createElement("td");
-var fatTd = document.createElement("td");
-var imcTd = document.createElement("td");
-
-nameTd.textContent = name;
-weightTd.textContent = weight;
-heightTd.textContent = height;
-fatTd.textContent = fat;
-imcTd.textContent = calcImc(weight, height);
-
-patientTr.appendChild(nameTd);
-patientTr.appendChild(weightTd);
-patientTr.appendChild(heightTd);
-patientTr.appendChild(fatTd);
-patientTr.appendChild(imcTd);
+var patientTr = buildTr(patient);
 
 // Add o paciente na tabela
 var table = document.getElementById("table-patient");
@@ -36,7 +17,7 @@ table.appendChild(patientTr);
 
 });
 
-function getPatientForm(form) {
+function getPatientForm(form){
     var patient = {
         name: form.name.value,
         weight: form.weight.value,
@@ -45,4 +26,28 @@ function getPatientForm(form) {
         imc: calcImc(form.weight.value, form.height.value)
     }
     return patient
+}
+
+function buildTr(patient){
+    var patientTr = document.createElement("tr");
+
+    var nameTd = document.createElement("td");
+    var weightTd = document.createElement("td");
+    var heightTd = document.createElement("td");
+    var fatTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nameTd.textContent = patient.name;
+    weightTd.textContent = patient.weight;
+    heightTd.textContent = patient.height;
+    fatTd.textContent = patient.fat;
+    imcTd.textContent = patient.imc;
+
+    patientTr.appendChild(nameTd);
+    patientTr.appendChild(weightTd);
+    patientTr.appendChild(heightTd);
+    patientTr.appendChild(fatTd);
+    patientTr.appendChild(imcTd);
+
+    return patientTr;
 }
