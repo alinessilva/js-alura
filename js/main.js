0b443ed538ea155c798efdf2d81b456db47ba720
alinessilva <alinedoze@gmail.com>
2018-10-04 17:27:00 -0300

    Add novos pacientes através do form

--- js/main.js
+++ js/main.js
@@ -34,3 +34,38 @@ for (var i = 0; i < patients.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var addBtn = document.getElementById("add-patient");
+addBtn.addEventListener("click", function(event){
+event.preventDefault();    
+
+var form = document.getElementById("add-form");
+
+var name = form.name.value;
+var weight = form.weight.value;
+var height = form.height.value;
+var fat = form.fat.value;
+
+var patientTr = document.createElement("tr");
+
+var nameTd = document.createElement("td");
+var weightTd = document.createElement("td");
+var heightTd = document.createElement("td");
+var fatTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+nameTd.textContent = name;
+weightTd.textContent = weight;
+heightTd.textContent = height;
+fatTd.textContent = fat;
+
+patientTr.appendChild(nameTd);
+patientTr.appendChild(weightTd);
+patientTr.appendChild(heightTd);
+patientTr.appendChild(fatTd);
+
+var table = document.getElementById("table-patient");
+
+table.appendChild(patientTr);
+
+});
