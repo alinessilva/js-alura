7a74c573ce9bed7e67fa464159b2d5d8cbed1416
alinessilva <alinedoze@gmail.com>
2018-10-14 17:40:13 -0300

    Add classe remove no filtro conforme pesquisa

--- js/filter.js
+++ js/filter.js
@@ -4,15 +4,21 @@ fildFilter.addEventListener("input", function(){
 
     var patients = document.querySelectorAll(".patient");
     
-    for (var i = 0; i < patients.length; i++){
-        var patient = patients[i];
-        var tdName = patient.querySelector(".info-name");
-        var name = tdName.textContent;
-        if (name != this.value){
-            patient.classList.add("invisible");
-        } else {
+    if (this.value.length > 0){
+        for (var i = 0; i < patients.length; i++){
+            var patient = patients[i];
+            var tdName = patient.querySelector(".info-name");
+            var name = tdName.textContent;
+            if (name != this.value){
+                patient.classList.add("invisible");
+            } else {
+                patient.classList.remove("invisible");
+            }
+        }
+    } else {
+        for (var i = 0; i < patients.length; i++){
+            var patient = patients[i];
             patient.classList.remove("invisible");
         }
     }
-    
 });
